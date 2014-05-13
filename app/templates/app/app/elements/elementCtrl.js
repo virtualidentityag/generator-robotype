6b521df0c004a8bb6811e1cca57b5111e8440cac
Timo Mayer <timo.mayer@MANDARINFISCH.virtual-identity.net>
2014-05-13 16:37:43 +0200

    update

--- app/templates/app/app/elements/elementCtrl.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('app')
-.controller('ElementCtrl', function ($scope, $routeParams, ElementService) {
-	var item = ElementService.getItem();
-	$scope.item = item.get({'id': $routeParams.id});
-});
--- app/templates/app/app/items/itemCtrl.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('app')
-	.controller('ItemCtrl', function ($scope, $routeParams, $route, ItemService) {
-		ItemService.getItem($routeParams.id).then(function(data) {
-			$scope.item = data;
-		});
-	});
